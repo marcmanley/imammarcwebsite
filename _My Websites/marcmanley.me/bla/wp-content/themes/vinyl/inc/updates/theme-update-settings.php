9655ee4c802842ddf8a067e7d3cfba814328ed8b
Marc Manley <marcmanleydev@gmail.com>
2021-06-21 15:42:00 -0700

    Trying to clear things out

--- _My Websites/marcmanley.me/bla/wp-content/themes/vinyl/inc/updates/theme-update-settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$vinyl_update_theme = new ThemeUpdateChecker(
-  'vinyl',
-  'http://updates.hipstertheme.com/themes/?action=get_metadata&slug=vinyl'
-);
--- _My Websites/marcmanley.me/scripts/ip.js
+++
@@ -1,5 +0,0 @@
-VIH_BackColor = "palegreen";
-VIH_ForeColor = "navy";
-VIH_FontPix = "16";
-VIH_DisplayFormat = "You are visiting from:<br>IP Address: %%IP%%<br>Host: %%HOST%%";
-VIH_DisplayOnPage = "yes";
